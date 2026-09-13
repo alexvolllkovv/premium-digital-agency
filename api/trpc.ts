ee961a70c9cfbb60d01fcc691ac7ee05841704d8
alexvolllkovv <alexvolllkovv@gmail.com>
2026-09-14 00:55:20 +0300

    Update trpc.ts

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,6 +1,6 @@
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../server/routers";
-import { createContext } from "../server/_core/context";
+import { appRouter } from "../server/routers.js";
+import { createContext } from "../server/_core/context.js";
 
 export default createExpressMiddleware({
   router: appRouter,
